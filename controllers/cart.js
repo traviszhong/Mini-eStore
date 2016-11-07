1191fbde9792af18a6cee5482b1f1056cc28cb4d
Ziyue <ziyuezhong@hotmail.com>
2016-11-06 21:41:28 -0800

    add the paypal function and shopping cart module

---
+++ controllers/cart.js
@@ -0,0 +1,51 @@
+'use strict';
+var Book = require('../models/bookModel');
+var Category = require('../models/categoryModel');
+
+
+module.exports = function (router) {
+    router.get('/', function (req, res) {
+        //Get the cart from session
+        var cart = req.session.cart;
+        var displayCart = {items:[],total:0};
+        var total = 0;
+
+        //Get total
+        for(var item in cart){
+          displayCart.items.push(cart[item]);
+          total += cart[item].subtotal;
+        }
+        displayCart.total = total.toFixed(2);
+        //render cart
+        res.render('cart/index',{
+          cart: displayCart
+        });
+    });
+    router.post('/:id', function (req, res) {
+        req.session.cart = req.session.cart || {};
+        var cart = req.session.cart;
+
+        Book.findOne({_id:req.params.id},function(err, book){
+          if(err){
+            console.log(err);
+          }
+          if(cart[req.params.id]){
+            cart[req.params.id].qty++;
+            cart[req.params.id].subtotal += cart[req.params.id].price;
+          }else{
+            cart[req.params.id] = {
+              item: book._id,
+              title: book.title,
+              price: book.price,
+              qty: 1,
+              subtotal: book.price
+            };
+          }
+          res.redirect('/cart');
+        });
+    });
+    router.get('/remove',function(req,res){
+      req.session.cart = {};
+      res.render('cart/index');
+    });
+};
